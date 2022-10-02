65347ab9ada37627f5ef4de7221dbc169f9f7d6d
MarkCodering <mark.chen.sstm@gmail.com>
2022-10-02 12:15:25 +0800

    set CORS policy to allow all sources

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 from flask_cors import CORS
 app = Flask(__name__)
 
-CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/')
 def index():
